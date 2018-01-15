1bf2cb49dfb3ec987cd788ccc0aafb5b333601e2
Gaurav <graheja1997@gmail.com>
2018-01-15 17:36:35 +0530

    header footer corrected

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(function(){
     $("button.rg-navbar-toggler").click(function(){
         $("div#collapsible-rg-navbar").toggle();     
     });
-})
+
+});
+    $(window).scroll(function () {
+    if( $(window).scrollTop() > $('.header').offset().top && !($('.header').hasClass('.header-fixed'))){
+      $('.header').addClass('.header-fixed');
+    } else if ($(window).scrollTop() == 0){
+      $('.header').removeClass('.header-fixed');
+    }
+});
