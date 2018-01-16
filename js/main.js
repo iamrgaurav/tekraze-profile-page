8453792915c0aba75b626d7c257a31b6b63e52b3
Gaurav <graheja1997@gmail.com>
2018-01-17 00:37:13 +0530

    Minified resources

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-$(function(){
-    $("button.rg-navbar-toggler").click(function(){
-        $("div#collapsible-rg-navbar").toggle();     
+$(function () {
+    $("button.rg-navbar-toggler").click(function () {
+        $("div#collapsible-rg-navbar").toggle();
     });
 
 
     $(window).scroll(function () {
-    if( $(window).scrollTop() > $('.rg-navbar-wrapper').offset().top && !($('.rg-navbar-wrapper').hasClass('header-fixed'))){
-      $('.rg-navbar-wrapper').addClass('header-fixed');
-    } else if ($(window).scrollTop() == 0){
-      $('.rg-navbar-wrapper').removeClass('header-fixed');
-    }
-});
-    $(".rg-nav-item > a").click(function(){
-            $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 500);
-    return false;
+        if ($(window).scrollTop() > $('.rg-navbar-wrapper').offset().top && !($('.rg-navbar-wrapper').hasClass('header-fixed'))) {
+            $('.rg-navbar-wrapper').addClass('header-fixed');
+        } else if ($(window).scrollTop() == 0) {
+            $('.rg-navbar-wrapper').removeClass('header-fixed');
+        }
+    });
+    $(".rg-nav-item > a").click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
     });
 });
